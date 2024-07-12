e61d4b817ec388d0bfaaecba121cfe6ded5a3373
devtoolsolutions <devtoolsolutions>
2024-07-12 07:26:00 -0300

    Iniciando implementação controller Cliente

---
+++ src/Bank.Client.Api/Controllers/MainController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Bank.Client.Api.Controllers;
+public abstract class MainController : ControllerBase
+{
+    protected ICollection<string> Errors = new List<string>();
+
+    protected ActionResult CustomResponse(object result = null)
+    {
+        if (ValidOperation())
+        {
+            return Ok(result);
+        }
+
+        return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+        {
+            { "Messages", Errors.ToArray() }
+        }));
+    }
+
+    protected ActionResult CustomResponse(ModelStateDictionary modelState)
+    {
+        var errors = modelState.Values.SelectMany(e => e.Errors);
+        foreach (var error in errors)
+        {
+            AddErrorToStack(error.ErrorMessage);
+        }
+
+        return CustomResponse();
+    }
+    
+    protected bool ValidOperation()
+    {
+        return !Errors.Any();
+    }
+
+    protected void AddErrorToStack(string error)
+    {
+        Errors.Add(error);
+    }
+
+    protected void CleanErrors()
+    {
+        Errors.Clear();
+    }
+}
---
+++ src/Bank.Client.Api/Controllers/v1/ClientController.cs
@@ -0,0 +1,20 @@
+using System.Net;
+using Bank.Core.Communication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Client.Api.Controllers.v1;
+
+[ApiController]
+[Route("api/v1/clients")]
+public class ClientController : MainController
+{
+    [HttpPost()]
+    [ProducesResponseType((int)HttpStatusCode.Created)]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType(typeof(ResponseResult), (int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> Register(object teste)
+    {
+        return Ok();
+    }
+
+}
