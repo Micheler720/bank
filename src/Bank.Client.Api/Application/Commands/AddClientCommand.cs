8a1ae7f2c78c96f927adfc092661d04efdfe4445
devtoolsolutions <devtoolsolutions>
2024-07-12 07:26:52 -0300

    Iniciando modelagem da classe cliente

---
+++ src/Bank.Client.Api/Application/Commands/AddClientCommand.cs
@@ -0,0 +1,25 @@
+using Bank.Core.Messages;
+using FluentValidation;
+
+namespace Bank.Client.Api.Application.Commands;
+
+public class AddClientCommand : Command
+{
+    public string? Name { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string? Document { get; set; }
+    public string? Email { get; set; }
+    public float CreditLimit { get; set; }
+}
+
+public class AddClientCommandValidation : AbstractValidator<AddClientCommand>
+{
+    public AddClientCommandValidation()
+    {
+        RuleFor(c => c.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(c => c.BirthDate).NotEmpty().WithMessage("BirthDate is required");
+        RuleFor(c => c.Document).NotEmpty().WithMessage("Document is required");
+        RuleFor(c => c.Email).NotEmpty().WithMessage("Email is required");
+        RuleFor(c => c.CreditLimit).NotEmpty().WithMessage("CreditLimit is required");
+    }
+}
---
+++ src/Bank.Client.Api/Domain/Client.cs
@@ -0,0 +1,19 @@
+using Bank.Core.DomainObjects;
+
+namespace Bank.Client.Api.Domain.Client;
+
+public class Client : Entity
+{
+    public string? Name { get; private set; }
+    public DateTime BirthDate { get; private set; }
+    public string? Document { get; private set; }
+    public string? Email { get; private set; }
+
+    public Client(string name, DateTime birthDate, string document, string email)
+    {
+        Name = name;
+        BirthDate = birthDate;
+        Document = document;
+        Email = email;
+    }
+}
