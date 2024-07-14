ae2c2b74639d4c3b72c4d0528b2a0af1f9f3c4d1
devtoolsolutions <devtoolsolutions>
2024-07-14 12:38:42 -0300

    Atualizando configurações consumer worker

--- src/Bank.Proposals.Worker/Configurations/MessageConfiguration.cs
+++ src/Bank.Proposals.Worker/Configurations/MessageConfiguration.cs
@@ -1,43 +1,24 @@
 using System.Diagnostics.CodeAnalysis;
+using Bank.Message;
 using Bank.Proposals.Worker.Services;
-using MassTransit;
 
-namespace Bank.Clients.Api.Configurations;
+namespace Bank.Proposals.Worker.Configurations;
 
 [ExcludeFromCodeCoverage]
 public static class MessageConfiguration
 {
-    public static void AddMessageConfig(this IServiceCollection services)
+    public static void AddMessageConfig(
+        this IServiceCollection services)
     {
-        var user = Environment.GetEnvironmentVariable("RABBITMQ_USER") ?? "";
-        var host = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "";
-        var password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") ?? "";
-
-        services.AddMassTransit(x =>
+        var consumers = new List<ConsumerConfiguration>
         {
-            x.AddConsumer<ClientRegistredConsumer>(); 
+            new() {
+                QueueName = "client-registred-queue",
+                ConsumerType = typeof(ClientRegistredConsumer)
+            }
+        };
 
-            x.UsingRabbitMq((context, cfg) =>
-            {
-                cfg.Host(host, "/", h =>
-                {
-                    h.Username(user);
-                    h.Password(password);
-                });
-
-                cfg.ReceiveEndpoint("client-registred-queue", e =>
-                {
-                    e.ConfigureConsumeTopology = false;
-                    e.AutoDelete = false;
-                    e.Durable = true;
-                    e.PrefetchCount = 10;
-                    e.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(60)));
-                    
-                    e.ConfigureConsumer<ClientRegistredConsumer>(context);
-                });
-
-                cfg.ConfigureEndpoints(context);
-            });
-        });
+        services.AddMessageBus(consumers);
+        
     }
 }
