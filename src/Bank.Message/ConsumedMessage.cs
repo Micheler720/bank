4d0738c9d931379638f5828a47e9f49c7e266bb6
devtoolsolutions <devtoolsolutions>
2024-07-14 21:06:25 -0300

    Ajustando nome propriedade ConsumedMessage

--- src/Bank.Message/ConsumedMessage.cs
+++ src/Bank.Message/ConsumedMessage.cs
@@ -4,13 +4,13 @@ namespace Bank.Message;
 
 public class ConsumedMessage<T> where T : class
 {
-    public T Message { get; private set; }
+    public T Data { get; private set; }
     public bool Redelivered { get; private set; }
     public int RetryCount { get; private set; }
 
     public ConsumedMessage(ConsumeContext<T> message)
     {
-        Message = message.Message;
+        Data = message.Message;
         Redelivered = message.ReceiveContext.Redelivered;
         RetryCount = message.GetRetryCount() + 1;
     }
