aa531620d4a0c527d3850b54aed921928e5d514e
devtoolsolutions <devtoolsolutions>
2024-07-17 20:30:02 -0300

    Incluindo cenário de exception na realização da proposta

--- src/services/Bank.Proposals.Worker/Infrastructure/HttpServices/ScoreHttpService.cs
+++ src/services/Bank.Proposals.Worker/Infrastructure/HttpServices/ScoreHttpService.cs
@@ -13,6 +13,9 @@ public class ScoreHttpService : IScoreHttpService
 
         if (raizDocument == "11")
             return Task.FromResult(random.Next(0, 300));
+        
+        if (raizDocument == "22")
+            throw new Exception("Erro ao buscar o score");
 
         return Task.FromResult(random.Next(301, 1000));
     }
